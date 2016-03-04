e47053a9839bf9c07b077ee7327721edb1413fbb
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-03-04 21:00:48 +0000

    print a table of factorials... very Charles Babbage

--- factorial.java
+++ factorial.java
@@ -2,32 +2,20 @@ import java.util.Scanner;
 
 public class factorial {
     public static void main(String[] args) {
-	while(true) {
-	    Scanner s = new Scanner(System.in);
-	    int n = getN(s);
-
-	    if(n<0) continue;
-	    
-	    long out = factorial(n);
-	    System.out.println(Long.toString(out));
-		}
-    }
-
-    public static int getN(Scanner s) {
-	int n = -1; // invalid number is our error flag
-	try {
-	    n = s.nextInt();
-	} catch(Exception InputMismatchException) {
-	    // and do nothing
+	System.out.println("N   Factorial\n--+--------------");
+	for(int i=1;i<=20;i++) {
+	    long out = factorial(i);
+	    System.out.printf("%-4d%d\n", i, out);
 	}
-
-	return n;
     }
-
+    
     public static long factorial(int x) {
 	if(x==0) {
+	    // 0! is defined as 1, details at
+	    // https://en.wikipedia.org/wiki/Factorial
 	    return 1;
 	} else {
+	    // 5! = 5 * 4! and 4! = 4 * 3!, so we can do this
 	    return x*factorial(x-1);
 	}
     }
